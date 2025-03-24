9ed0712a5b73f5dbf79a1da155a1245eb98be6b0
Mark Dixon <mdixon@plymouth.ac.uk>
2025-03-24 23:36:14 +0000

    Added Java Spring Boot application.

---
+++ src/main/java/uk/md/MainController.java
@@ -0,0 +1,12 @@
+package uk.md;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @GetMapping("/hello")
+    String index(){
+        return Message.getMessage();
+    }
+}
---
+++ src/main/java/uk/md/Message.java
@@ -0,0 +1,7 @@
+package uk.md;
+
+public class Message {
+    public static String getMessage(){
+        return "Hello there!!";
+    }
+}
